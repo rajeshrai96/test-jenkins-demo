427b97912c7514871fe8e7e2f9553f7ccd553110
rajeshrai1986 <rajeshtct96@gmail.com>
2023-11-23 10:26:40 +0530

    Added logs in test claa

--- src/test/java/com/demo/TestJenkinsDemoApplicationTests.java
+++ src/test/java/com/demo/TestJenkinsDemoApplicationTests.java
@@ -1,13 +1,18 @@
 package com.demo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TestJenkinsDemoApplicationTests {
-
+private Logger logger= LoggerFactory.getLogger(TestJenkinsDemoApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("This is Jenkins test logger statement..................");
+		Assertions.assertEquals(true,true);
 	}
 
 }
