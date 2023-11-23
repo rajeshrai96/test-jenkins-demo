b353932d66c4bf29c981aaf267e473029d4e824d
rajeshrai1986 <rajeshtct96@gmail.com>
2023-11-23 08:08:37 +0530

    added new controller

---
+++ src/main/java/com/demo/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/message")
+    public String getMessage(){
+        return "This is my first Jenkins program";
+    }
+}
