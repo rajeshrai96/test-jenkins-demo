f839e5030a6f73f55d13b0026d37cd76fab1c142
rajeshrai1986 <rajeshtct96@gmail.com>
2023-11-23 10:52:27 +0530

    Added logs in main class

--- src/main/java/com/demo/TestJenkinsDemoApplication.java
+++ src/main/java/com/demo/TestJenkinsDemoApplication.java
@@ -1,13 +1,22 @@
 package com.demo;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestJenkinsDemoApplication {
+    private Logger logger = LoggerFactory.getLogger(TestJenkinsDemoApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestJenkinsDemoApplication.class, args);
-	}
+    @PostConstruct
+    public void intt() {
+        logger.info("Application executed...................");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJenkinsDemoApplication.class, args);
+    }
 
 }
